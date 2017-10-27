d96c69208bf76b405330493c38fa05dde34ab3b7
David Hale <david.hale7@gmail.com>
2017-10-27 10:08:08 -0700

    add animal constructor

--- app/animal-model.ts
+++ app/animal-model.ts
@@ -1,3 +1,3 @@
 export class Animal {
-  constructor() { }
+  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public like: string, public dislike: string) { }
 }
